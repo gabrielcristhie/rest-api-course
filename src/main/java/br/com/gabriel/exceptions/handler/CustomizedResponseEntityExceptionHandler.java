62b4c29752c3316887bc74b08ec5566c5426ffe2
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2023-10-31 22:15:20 -0300

    adicionando autenticacao parte 1

--- src/main/java/br/com/gabriel/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/gabriel/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.gabriel.exceptions.ExceptionResponse;
+import br.com.gabriel.exceptions.InvalidJwtAuthenticationException;
 import br.com.gabriel.exceptions.RequiredObjectIsNullException;
 import br.com.gabriel.exceptions.ResourceNotFoundException;
 
@@ -48,6 +49,16 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				new Date(), ex.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+			
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException (
+			Exception ex, WebRequest request) {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 	
 }
